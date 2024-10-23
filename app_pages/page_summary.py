37989834e59b23272f5de8bebaff693895335826
Callum Priestman <callum.priestman@hotmail.co.uk>
2024-10-23 12:34:33 +0000

    project hypothesis page added

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
 def page_summary():
     st.write("### Project Summary")
@@ -9,7 +10,9 @@ def page_summary():
         f"* This fungus can easily be spread by insects or wind pushing the fungus spores airborn\n"
         f"* The process for manually identifying these trees can be very tedious and time consuming for the business\n"
         f"* Multiple samples are collected via hand from the trees, assessed and then if"
-        f" necessary a compound is applied to the diseased tree, aiming to kill the fungus."
+        f" necessary a compound is applied to the diseased tree, aiming to kill the fungus.\n"
+        f"* The given dataset contains 4208 samples with the data being split 50/50/n"
+        f" between healthy and powdered mildew"
     )
 
     st.success(
